cd91823ecbf8fd8731888ced377cf176251db774
darren hardy <darren@aleph-null.co.uk>
2025-03-28 20:19:45 +0000

    28 March 2025

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 const myDiv = document.getElementById("myDiv");
-var currentcharacter;
-const storefulltext = ["This is senctence 1", "This is senctence 2", "This is senctence 3"];
-var currentsentence;
-var typingspeed;
-var totalcharacters;
-const blinkcursorate = 500;
+var currentCharacter = 0;
+const fullTextArray = ["This is senctence 1", "This is senctence 2", "This is senctence 3"];
+var currentSentence = 0;
+var typingSpeed = 100;
+var totalCharacters = 0;
+const blinkCursoRate = 500;
 
-function PETscreen() {
-    myDiv.innerHTML += fullTextArray.charAt(index); // Add one character
-    index++;
-    setTimeout(typeWriter, typingSpeed); // Call the function again after a delay
-    var totalcharacters =  storefulltext.length;
-    console.log(totalcharacters);
+function PetScreen() {
+    myDiv.innerHTML += fullTextArray.charAt(currentCharacter); // Add one character
+    currentCharacter++;
+    setTimeout(PetScreen, typingSpeed); // Call the function again after a delay
+    var totalcharacters =  fullTextArray.length;
+    console.log(totalCharacters);
     
 
 }
 
-PETscreen();
+PetScreen();
