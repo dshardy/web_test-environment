47f248bb8c1e74a8c2a63cea6005e4246d72c78b
Darren Hardy <darren.hardy@emobyus.co.uk>
2025-03-28 20:23:02 +0000

    Work laptop 28 March 2025

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
+
+
 const myDiv = document.getElementById("myDiv");
-var currentcharacter;
-const storefulltext = ["This is senctence 1", "This is senctence 2", "This is senctence 3"];
+var currentcharacter = 0;
+const fullTextArray = ["This is senctence 1", "This is senctence 2", "This is senctence 3"];
 var currentsentence;
-var typingspeed;
-var totalcharacters;
+var typingspeed = 100;
+var index = 0;
+var totalcharacters = 0;
 const blinkcursorate = 500;
 
-function PETscreen() {
+function PetScreen() {
     myDiv.innerHTML += fullTextArray.charAt(index); // Add one character
     index++;
-    setTimeout(typeWriter, typingSpeed); // Call the function again after a delay
-    var totalcharacters =  storefulltext.length;
-    console.log(totalcharacters);
-    
+    setTimeout(PetScreen, typingspeed); // Call the function again after a delay
+    var totalcharacters =  fullTextArray.length;
 
 }
 
-PETscreen();
+PetScreen();
