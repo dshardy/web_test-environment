75d2f7001d4456d4b9c7f5eba57c2804bc047736
Darren Hardy <darren.hardy@emobyus.co.uk>
2025-03-29 01:48:33 +0000

    PetScreen not working but 'type' demo working in tandem

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let i = 0;
+let placeholder = "";
+//const fullTextArray = ["This is senctence 1", "This is senctence 2", "This is senctence 3"];
+const txt = "example@domain.com";
+var speed = 100;
+
+/* const myDiv = document.getElementById("myDiv");
+var currentcharacter = 0;
+var currentsentence = 0;
+var totalcharacters = 0;
+const blinkcursorate = 500;  */
+
+function PetScreen(){
+    placeholder += txt.charAt(i); // Add one character
+    // document.getElementById("myDiv").textContent=placeholder;
+    document.getElementById("myDiv").setAttribute("myDiv", placeholder);
+    i++;
+    setTimeout(PetScreen,speed); // Call the function again after a delay
+}
+
+function type(){
+    placeholder += txt.charAt(i);
+    document.getElementById("email-id").setAttribute("placeholder",placeholder);
+    i++;
+    setTimeout(type,speed);
+}
+
+//PetScreen();
+type();
