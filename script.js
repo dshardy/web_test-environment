b441d01ee1661c04de4f639df76a7d1a5e33ba89
darren hardy <darren@aleph-null.co.uk>
2025-03-31 08:11:40 +0100

    31 March 2025

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let i = 0; // Index for characters within a sentence
+let currentcharacter = 0; // Index for characters within a sentence
 let sentenceIndex = 0; // Index for the current sentence in fullTextArray
 let placeholder = "";
 const fullTextArray = ["This is sentence 1", "This is sentence 2", "This is sentence 3"];
@@ -7,16 +7,16 @@ var speed = 170;
 
 function PetScreen() {
   // Check if we've finished the current sentence
-  if (i < fullTextArray[sentenceIndex].length) {
+  if (currentcharacter < fullTextArray[sentenceIndex].length) {
     // Add one character from the current sentence
-    placeholder += fullTextArray[sentenceIndex].charAt(i);
+    placeholder += fullTextArray[sentenceIndex].charAt(currentcharacter);
     document.getElementById("myDiv").textContent = placeholder;
-    i++;
+    currentcharacter++;
     setTimeout(PetScreen, speed); // Continue typing the current sentence
   } else {
     // We've finished the current sentence
     placeholder = ""; // Reset placeholder for the next sentence
-    i = 0; // Reset character index for the next sentence
+    currentcharacter = 0; // Reset character index for the next sentence
     sentenceIndex++; // Move to the next sentence
 
     // Check if we've finished all sentences
@@ -30,6 +30,7 @@ function PetScreen() {
 }
 
 PetScreen();
+
 
 
 
